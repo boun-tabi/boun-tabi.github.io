1911289b396920ce8b8605c9e1f0ba16e1195426
Gökçe Uludoğan <gokceuludogan@gmail.com>
2025-12-02 16:56:34 +0300

    Fix paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://boun-tabi-lifelu.github.io',
-  base: '/boun-tabi-dev/',
+  site: 'https://boun-tabi.github.io',
+  base: '/',
   outDir: './dist',
   integrations: [
     tailwind(),
